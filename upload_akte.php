6cba1e51a79ef690ee3b5f5e8ecc5e9a8aee677c
MaunaSap <fiki_maulana@teknokrat.ac.id>
2024-01-25 15:23:53 +0700

    FIKI: UPDATE SINTAK

--- upload_akte.php
+++ upload_akte.php
@@ -1,37 +1,35 @@
-<?php include 'head.php'; 
+<?php 
+include 'head.php';
 
 if(isset($_POST['simpanakte'])){
-  $id_pendaftar=$_SESSION['id'];
+  $id_pendaftar = $_SESSION['id'];
 
   $nama   = $_FILES['akte']['name'];
   $size   = $_FILES['akte']['size'];
   $error  = $_FILES['akte']['error'];
-  $asal = $_FILES['akte']['tmp_name'];
-  
-  $lokasi_foto="img/dokumen/akte/" . $nama;
+  $asal   = $_FILES['akte']['tmp_name'];
+
+  $lokasi_foto = "img/dokumen/akte/" . $nama;
   $format = pathinfo($nama, PATHINFO_EXTENSION);
 
   if($error === 0){
+    if($size > 9000){
+      if(in_array($format, ['jpg', 'png', 'JPG', 'PNG'])){
+        move_uploaded_file($asal, "img/dokumen/akte/".$nama);
 
-    if ($size > 9000){ 
-    
-          if($format === "jpg" || $format === "png" || $format === "JPG" || $format === "PNG" ){
-            
-            if(simpanakte($id_pendaftar,$lokasi_foto)){
-              echo "<script>window.location.href='dokumen.php'</script>";
-              }else{
-                echo mysqli_error($konek);
-              } 
-    
-            move_uploaded_file($asal, "img/dokumen/akte/".$nama);
-          }else{
-            echo "Maaf format filenya harus jpg/png ";
-          }
-    
-      }else{
-        echo "file terlalu besar";
+        if(simpanakte($id_pendaftar, $lokasi_foto)){
+          echo "<script>window.location.href='dokumen.php'</script>";
+        } else {
+          echo "Error: Unable to save data.";
+        }
+      } else {
+        echo "Maaf format filenya harus jpg/png ";
       }
-  }else "Ada kesalahan saat upload";
+    } else {
+      echo "file terlalu besar";
+    }
+  } else {
+    echo "Ada kesalahan saat upload";
+  }
 }
-
 ?>
